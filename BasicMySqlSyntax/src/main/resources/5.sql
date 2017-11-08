31f77bf9641d3d06796a18d3ad50829e38f0ecc8
kurotkin <kurotkin@users.noreply.github.com>
2017-11-08 13:03:51 +0300

    Finished all scripts

--- BasicMySqlSyntax/src/main/resources/5.sql
+++ BasicMySqlSyntax/src/main/resources/5.sql
@@ -1,18 +1,10 @@
 # Найти клиента (customer), которая приносит меньше всего прибыли компании (company) для каждой из компаний
 USE BasicMySqlSyntax;
 
-SELECT componies.name, customers.firs_name, customers.last_name FROM componies, customers WHERE
-
-
-SELECT projects.name, sum(developers.salary)
-FROM developers, developer_projects, projects
-WHERE developers.id = developer_projects.developer_id AND developer_projects.projects_id = projects.id
-GROUP BY projects.name
-ORDER BY sum(developers.salary) DESC
-LIMIT 1;
-
-
-
 SELECT companies.name, customers.firs_name, customers.last_name
-FROM companies, companies_projects, customers_projects, customers
-WHERE companies.id = companies_projects.companies_id AND companies_projects.projects_id
+FROM companies, companies_projects, customers_projects, projects, customers
+WHERE companies.id = companies_projects.companies_id
+      AND companies_projects.projects_id = customers_projects.projects_id
+      AND customers_projects.customers_id = customers.id
+GROUP BY customers.id
+ORDER BY sum(projects.cost) ASC;
--- BasicMySqlSyntax/src/main/resources/6.sql
+++ BasicMySqlSyntax/src/main/resources/6.sql
@@ -1,4 +1,8 @@
 # Вычислить, среднюю ЗП программистов в проекте, который приносит наименьшую прибыль
 USE BasicMySqlSyntax;
 
-
+SELECT AVG(developers.salary)
+FROM developers, developer_projects, projects
+WHERE developers.id = developer_projects.developer_id AND projects.id = developer_projects.projects_id
+ORDER BY projects.cost DESC
+LIMIT 1;
